e8760649b4c15a0bf45f7364765b1ffb6b8648d3
Kevin giordani <giordani.kevin.02@gmail.com>
2018-01-14 00:16:54 +0100

    Add files via upload

--- presenceDetector.py
+++ presenceDetector.py
@@ -8,8 +8,6 @@ class EntranceUltrasonic(threading.Thread):
 
     def __init__(self,host,port):
         threading.Thread.__init__(self)
-        # Connect the Grove LED to digital port D3
-        self.led = 3
         # Connect the Grove Ultrasonic Ranger to digital port D4
         self.ultrasonic_ranger = 4
 
@@ -35,11 +33,9 @@ class EntranceUltrasonic(threading.Thread):
     def run(self):
         self.isHome = False
         self.performAction("isNotHome")
-        pinMode(self.led,"OUTPUT")
 
         lastValues = [50]*10
         print lastValues
-        digitalWrite(self.led,0)
         while True:
             try:
                 time.sleep(1)
@@ -47,27 +43,22 @@ class EntranceUltrasonic(threading.Thread):
                 lastValues.append(ultrasonicRead(self.ultrasonic_ranger))
                 if(len(lastValues) > 10):
                     lastValues = lastValues[1:]
-                # Read distance value from Ultrasonic
                 if self.isHomeFunction(lastValues):
                     print "Keys are in place !"
                     if not(self.isHome):
-                        digitalWrite(self.led,1)
                         self.performAction("isHome")
                         self.isHome = True
                 else:
                     print "Keys aren't in place !"
                     if self.isHome:
-                        digitalWrite(self.led,0)
                         self.performAction("isNotHome")
                         self.isHome = False
-            except KeyboardInterrupt:   # Turn LED off before stopping
-                digitalWrite(self.led,0)
+            except KeyboardInterrupt:
                 break
             except IOError:
                 print "Error"
 
 port = int(raw_input("Entrez le port d'ecoute : "))
 host = raw_input("Entrez l'hote' : ")
-host = "192.168.1.81"
 ultrasonic = EntranceUltrasonic(host,port);
 ultrasonic.start()
